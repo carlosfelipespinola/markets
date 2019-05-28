89aef165cd6503fcd244aae950e0c62589840eb4
Carlos Felipe <carlosfelipespiinola@hotmail.com>
2019-05-28 14:50:03 -0300

    modulo de supermercados e compradores importado no app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HomeModule } from './home/home.module';
+import { BuyersModule } from './buyers/buyers.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HomeModule,
+    BuyersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
