c12443f48e72f1da81530a6f3c0ad4e7b51231df
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-05-28 19:02:26 -0300

    navegação da lista de supermercados para um supermercado especifico ao clicar no supermercado

--- src/app/buyers/pages/home-page/home-page.component.ts
+++ src/app/buyers/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MarketData } from 'src/app/markets/data_classes/MarketData';
-import { MatBottomSheet, MatBottomSheetRef } from '@angular/material/bottom-sheet';
-import { FilterMarketsComponent } from 'src/app/markets/components/filter-markets/filter-markets.component';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { BuyersHomePageBottomSheetComponent } from '../../components/buyers-home-page-bottom-sheet/buyers-home-page-bottom-sheet.component';
-import { Observable, BehaviorSubject, Subscription } from 'rxjs';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { MarketFilterData } from 'src/app/markets/data_classes/MarketFilterData';
+import { Router } from '@angular/router';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'buyers-home-page',
@@ -16,7 +17,6 @@ export class HomePageComponent implements OnInit, OnDestroy {
   private _marketFilters = new MarketFilterData();
   private marketFiltersSubscription: Subscription = null;
   public marketFilters = new BehaviorSubject<MarketFilterData>(this._marketFilters);
-  
 
   public marketsFound: Array<MarketData> = [
     new MarketData({tradeName: "Supermercado Crimeia", rating: 4}),
@@ -24,7 +24,10 @@ export class HomePageComponent implements OnInit, OnDestroy {
     new MarketData({tradeName: "Supermercado Monteiro", rating: 3.5})
   ];
 
-  constructor(private bottomSheet: MatBottomSheet) { }
+  constructor(
+    private bottomSheet: MatBottomSheet,
+    private buyersNavigationService: NavigationService
+  ) { }
 
   ngOnInit() {
     this.marketFiltersSubscription = this.marketFilters.subscribe((value) => {
@@ -38,8 +41,9 @@ export class HomePageComponent implements OnInit, OnDestroy {
     }
   }
 
-  public onMarketClick(market: MarketData){
-    
+  public onMarketClick(market: MarketData) {
+    const id = 123;
+    this.buyersNavigationService.toBuyProductsAtMarketPage(id);
   }
 
   public onFabClick(){
