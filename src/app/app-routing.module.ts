b9e20fb0927aaabba1feac54178ff3137988b895
Carlos Felipe <carlosfelipespiinola@hotmail.com>
2019-05-28 14:50:32 -0300

    redirecionamento de / para /home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { RouterModule } from '@angular/router';
+import { homeRoute } from './home/home.routes';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([{path: '', redirectTo: homeRoute.path, pathMatch: 'full'}])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
