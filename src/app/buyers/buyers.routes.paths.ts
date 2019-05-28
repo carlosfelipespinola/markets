c0359fa5f785c0a2327095252931e8f19a95c1b3
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-05-28 18:56:50 -0300

    paths das rotas colocadas em arquivo diferente para evitar dependencia ciruclar;

---
+++ src/app/buyers/buyers.routes.paths.ts
@@ -0,0 +1,3 @@
+
+export const buyersHomePageRoutePath = 'buyers/find-market';
+export const buyersBuyProductAtMarketPageRoutePath = 'buyers/buy-products-at-market/:id';
--- src/app/buyers/buyers.routes.ts
+++ src/app/buyers/buyers.routes.ts
@@ -1,8 +1,20 @@
 import { Route, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { BuyProductsAtMarketPageComponent } from './pages/buy-products-at-market-page/buy-products-at-market-page.component';
+import { buyersHomePageRoutePath, buyersBuyProductAtMarketPageRoutePath } from './buyers.routes.paths';
 
-export const buyersHomePage: Route = {path: 'buyers/find-market', component: HomePageComponent}
+
+export const buyersHomePage: Route = {
+  path: buyersHomePageRoutePath,
+  component: HomePageComponent
+};
+
+export const buyersBuyProductAtMarketPage: Route = {
+  path: buyersBuyProductAtMarketPageRoutePath,
+  component: BuyProductsAtMarketPageComponent
+};
 
 export const buyersRoutes: Routes = [
-  buyersHomePage
+  buyersHomePage,
+  buyersBuyProductAtMarketPage
 ]
