0c4d1b686aecf91be630cd1493e039c642ec14fa
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-06-01 11:15:23 -0300

    formulário para cadastro de produtos criado

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductFormComponent],
   imports: [
     CommonModule,
     MatCardModule
