22425d67e9d62e0de96c6d46b9f40b254d3cc218
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-06-01 12:35:54 -0300

    migração do servidor para o firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,27 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HomeModule } from './home/home.module';
 import { BuyersModule } from './buyers/buyers.module';
 
+import { AngularFireModule } from '@angular/fire';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCZLMZthXxXiUMhRuIO1RA_JvHrarezlHg',
+  authDomain: 'supermercadosonline-14c09.firebaseapp.com',
+  databaseURL: 'https://supermercadosonline-14c09.firebaseio.com',
+  projectId: 'supermercadosonline-14c09',
+  storageBucket: 'supermercadosonline-14c09.appspot.com',
+  messagingSenderId: '1035335065182',
+  appId: '1:1035335065182:web:83101fecbda09663'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     HomeModule,
     BuyersModule,
   ],
