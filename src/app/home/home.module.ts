b10a345e61dddf581da39ef9f6a75ed6efaba0c7
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-06-04 07:42:05 -0300

    component não utilizado removido

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,7 +6,6 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { AuthModule } from '../auth/auth.module';
 import {MatCardModule} from '@angular/material/card';
 import { HomeRoutingModule } from './home.routing';
-import { LoginFormComponentModule } from '../auth/components/login-form/login-form-component.module';
 import { MatChipsModule } from '@angular/material/chips';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,7 +17,6 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    LoginFormComponentModule,
     MatToolbarModule,
     MatTabsModule,
     MatCardModule,
