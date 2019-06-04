482a63e5b885283c6bf2708160e4f6fe0e30a271
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-06-04 09:00:51 -0300

    correção estilo card

--- src/app/ui/cards/cards.module.ts
+++ src/app/ui/cards/cards.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OneByTwoCardComponent } from './one-by-two-card/one-by-two-card.component';
 import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [OneByTwoCardComponent],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    FlexLayoutModule
   ],
   exports: [OneByTwoCardComponent]
 })
