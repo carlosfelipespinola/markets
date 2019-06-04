9b37ffc39ded4dc94682d7a28139da3978269038
Carlos Felipe <carlosfelipespiinola@gmail.com>
2019-06-04 07:51:10 -0300

    component não utlizado removido

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
-import { ProductFormComponent } from './components/product-form/product-form.component';
 
 @NgModule({
-  declarations: [ProductFormComponent],
+  declarations: [],
   imports: [
     CommonModule,
     MatCardModule
